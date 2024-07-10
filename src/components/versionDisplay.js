c1e044ce279afc1e3c237c43f0a58ef3887aabbe
Lyza Sadi <lyza@MacBook-Air-de-Lyza-2.local>
2024-07-10 09:52:41 +0200

    add versionDisplay

---
+++ src/components/versionDisplay.js
@@ -0,0 +1,12 @@
+import React from "react";
+import packageJson from "../../package.json";
+
+const VersionDisplay = () => {
+  return (
+    <div className="version-display">
+      Version: {packageJson.version}
+    </div>
+  );
+};
+
+export default VersionDisplay;
